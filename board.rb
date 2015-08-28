a7d23c18bd2c922e0e967465a787922e99554b0b
App Academy Student <student@appacademy.io>
2015-08-28 13:18:39 -0700

    First Draft of Minesweeper

---
+++ board.rb
@@ -0,0 +1,76 @@
+require_relative 'tile'
+
+class Board
+  attr_accessor :game_over, :num_flag
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(9) {Array.new(9)}
+    @game_over = false
+    @num_flag = 0
+  end
+
+
+
+  def populate_grid
+    grid.each_with_index do |row, row_idx|
+      row.each_with_index do |cell, col_idx|
+        cell = Tile.new(self, [row_idx, col_idx])
+      end
+    end
+    puts grid
+    add_bombs
+  end
+
+  def [](pos)
+    grid[pos[0]][pos[1]]
+  end
+
+  def add_bombs
+    9.times do
+      bomb_status = grid.sample.sample.bomb_status
+      until !bomb_status
+        bomb_status = grid.sample.sample.bomb_status
+      end
+      bomb_status = true
+    end
+  end
+
+  def render
+    grid.each do |row|
+      row.each do |cell|
+        print  "#{cell.display} "
+      end
+      print "\n"
+    end
+  end
+
+  def play
+    until game_over
+      pos = prompt
+      flag = prompt_flag
+      self[pos].reveal(flag)
+    end
+  end
+
+  def prompt
+    puts "Please choose a location"
+    pos = gets.chomp.split(',').map {|num| num.to_i}
+  end
+
+  def prompt_flag
+    puts "Do you want to flag? (enter 'F') Or unflag? (enter 'U') Or none (hit enter)"
+    value = gets.chomp.downcase
+    if value == 'f'
+      num_flag += 1
+      return true
+    elsif value == 'u'
+      num_flag -= 1
+      return false
+    else
+      return nil
+    end
+  end
+
+
+end
---
+++ tile.rb
@@ -0,0 +1,59 @@
+class Tile
+
+  MOVE = [[0,1],[1,0],[-1,0],[0,-1],[1,1],[-1,-1],[-1,1],[1,-1]]
+
+  attr_accessor :reveal_status, :bomb_status
+
+  def initialize(board, position, bomb_status = false, reveal_status = false)
+    @board = board
+    @reveal_status = reveal_status
+    @bomb_status = bomb_status
+    @flagged = false
+    @position = position
+    @display = '*'
+  end
+
+  def reveal(flag)
+    if !(flag.nil?)
+      flagged = true if flag
+      flagged = false if flag == false
+    elsif bomb_status
+      print "BOOM"
+      board.game_over = true
+    elsif board.flatten.all? {|tile| tile.reveal_status || tile.flagged} &&
+        board.num_flag < 10
+      puts "Congrats!"
+      board.game_over = true
+    else
+      reveal!
+    end
+  end
+
+  def reveal!
+    if neighbor_bomb_count > 0
+      reveal_status = true
+      display = neighbor_bomb_count.to_s
+      return
+    end
+    display = '_'
+    neighbors.each {|neighbor| neighbor.reveal!}
+  end
+
+  def neighbors
+    neighbors_pos = []
+    MOVE.each do |move|
+      possible_neigbhor = [move[0] + position[0] , move[1]+position[1]]
+      if possible_neigbhor.all? { |cord| cord.between?(0,8) }
+        neighbors_pos << possible_neigbhor
+      end
+    end
+    neighbors = neighbors_pos.map {|pos| board[pos]}
+  end
+
+  def neighbor_bomb_count
+    neighbors.count {|tile| tile.bomb_status}
+  end
+
+
+
+end
