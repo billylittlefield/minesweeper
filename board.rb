7d46bda7c5eb26bafd9908d68f42f50384805aaf
App Academy Student <student@appacademy.io>
2015-08-28 16:42:58 -0700

    Minesweeper finished, including save/load function

--- board.rb
+++ board.rb
@@ -7,19 +7,6 @@ class Board
     @grid = Array.new(9) {Array.new(9)}
   end
 
-  def play
-    populate_grid
-    until lost? || won?
-      system 'clear'
-      render
-      pos = prompt_location
-      next if pos.empty?
-      target = self[pos]
-      prompt_flag ? target.flag : target.reveal
-    end
-    lost? ? puts("BOOM") : puts("You got lucky.")
-  end
-
   def [](pos)
     grid[pos[0]][pos[1]]
   end
@@ -27,10 +14,6 @@ class Board
   def []=(pos, mark)
     grid[pos[0]][pos[1]] = mark
   end
-
-  private
-
-  attr_reader :grid
 
   def populate_grid
     grid.each_with_index do |row, row_idx|
@@ -50,6 +33,22 @@ class Board
     grid.flatten.all? { |tile| tile.bombed || tile.revealed }
   end
 
+  def render
+    puts "Welcome to Minesweeper. Prepare to do a good job."
+    puts "  #{(0..8).to_a.join(' ')}"
+    grid.each_with_index do |row, row_idx|
+      print "#{row_idx} "
+      row.each do |cell|
+        print  "#{cell.display} "
+      end
+      print "\n"
+    end
+  end
+
+  private
+
+  attr_reader :grid
+
   def add_bombs
     9.times do
       position = grid.sample.sample
@@ -61,44 +60,4 @@ class Board
     nil
   end
 
-  def render
-    puts "Welcome to Minesweeper. Prepare to do a good job."
-    puts "  #{(0..8).to_a.join(' ')}"
-    grid.each_with_index do |row, row_idx|
-      print "#{row_idx} "
-      row.each do |cell|
-        print  "#{cell.display} "
-      end
-      print "\n"
-    end
-    nil
-  end
-
-  def prompt_location
-    puts "Please choose a location"
-    puts "If you'd like to save, enter 'save'"
-    input = gets.chomp
-    if input == 'save'
-      puts "Please enter a filename."
-      filename = gets.chomp
-      File.write(filename, self.to_yaml)
-      Kernel.exit
-    else
-      pos.split(',').map {|num| num.to_i}
-    end
-  end
-
-  def prompt_flag
-    puts "Do you want to flag? (enter 'F') Or unflag? (enter 'U') Or none (hit enter)"
-    value = gets.chomp.downcase
-    if value == 'f' || value == 'u'
-      return true
-    end
-    false
-  end
-end
-
-if __FILE__ == $PROGRAM_NAME
-  board = Board.new
-  board.play
 end
---
+++ game.rb
@@ -0,0 +1,52 @@
+require_relative 'board'
+
+class Minesweeper
+
+  attr_reader :board
+
+  def initialize(board)
+    @board = board
+    board.populate_grid
+  end
+
+  def play
+    until board.lost? || board.won?
+      system 'clear'
+      board.render
+      pos = prompt_location
+      next if pos.empty?
+      target = board[pos]
+      prompt_flag ? target.flag : target.reveal
+    end
+    board.lost? ? puts("BOOM") : puts("You got lucky.")
+  end
+
+  def prompt_location
+    puts "Please choose a location"
+    puts "If you'd like to save, enter 'save'"
+    input = gets.chomp
+    if input == 'save'
+      puts "Please enter a filename."
+      filename = gets.chomp
+      File.write(filename, self.to_yaml)
+      Kernel.exit
+    else
+      input.split(',').map {|num| num.to_i}
+    end
+  end
+
+  def prompt_flag
+    puts "Do you want to flag? (enter 'F') Or unflag? (enter 'U') Or none (hit enter)"
+    value = gets.chomp.downcase
+    if value == 'f' || value == 'u'
+      return true
+    end
+    false
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  board = Board.new
+  game = ARGV[0] ? YAML.load_file(ARGV.shift) : Minesweeper.new(board)
+  game.play
+end
